0b04fd5602f39725d8eec732bc01c9f8d6a1b2c0
Maxime Challon <maxime.challon@gmail.com>
2020-06-09 17:16:32 +0200

    graphique temporel précis au jour

--- app/routes/generals.py
+++ app/routes/generals.py
@@ -1,12 +1,32 @@
 from flask import render_template, flash, redirect, request
 from ..app import app
+import altair
+from ..models.graphiques import classe_graphiques
+import pandas as pd
 
 @app.route("/")
 def accueil():
-    """ Route permettant l'affichage d'une page accueil
-    """
     return render_template("pages/accueil.html")
 
 @app.route("/cartographie")
 def cartographie():
 	return render_template("pages/cartographie.html")
+
+@app.route("/graphiques")
+def graphiques():
+	return render_template("pages/graphiques.html")
+
+@app.route("/graphiques/temporels")
+def temporels():
+	data = pd.read_csv(f"./app/statics/data/inventaire_complet.csv")
+	data["Date_inventaire"] = pd.to_datetime(data["Date_inventaire"], format="%Y/%m/%d")
+	#ajout colonne occurences
+	data["occurences"] = 1
+
+	# Group data by date
+	general = data.groupby("Date_inventaire", as_index=False).sum()
+	
+	chart = classe_graphiques.generate_temporels(general, "Mois et jour")
+	return chart.to_json()
+
+#@app.route("/graphiques/cumulatifs")
