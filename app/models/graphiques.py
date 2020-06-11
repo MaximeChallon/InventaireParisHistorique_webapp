783bdd1514818de5ba5d1d1c32fb30628c5a7330
Maxime Challon <maxime.challon@gmail.com>
2020-06-11 18:40:56 +0200

    ajout de données par mois pour les graphiques

--- app/models/graphiques.py
+++ app/models/graphiques.py
@@ -2,16 +2,16 @@ import altair as alt
 import pandas as pd
 
 class classe_graphiques():
-	def generate_temporels(
-    	data: pd.DataFrame,
+	def generate_temporels_line(
+		data: pd.DataFrame,
     	x_title: str,
     	padding: int = 5,
     	width: int = 700,
-    	height: int = 500,
+    	height: int = 500
 		):
 		return (
 			alt.Chart(data)
-    		.mark_line(point={"size": 70})
+    		.mark_line(point={"size": 70}, color='red')
     		.encode(
             	x=alt.X("Date_inventaire", title=x_title),
             	y=alt.Y(f"occurences", title="Nombre de photos inventoriées"),
@@ -23,4 +23,26 @@ class classe_graphiques():
         	.configure_scale(continuousPadding=padding)
         	.properties(width=width, height=height)
         	.interactive()
-    	)
+            )
+
+	def generate_temporels_bar(
+		data: pd.DataFrame,
+        x_title: str,
+        padding: int = 5,
+        width: int = 700,
+        height: int = 500):
+		return (
+            alt.Chart(data)
+            .mark_bar(color='red')
+            .encode(
+                x=alt.X("Date_inventaire", title=x_title),
+                y=alt.Y(f"occurences", title="Nombre de photos inventoriées"),
+                tooltip=[
+                alt.Tooltip(f"occurences", title="Nombre de photos inventoriées"),
+                alt.Tooltip("Date_inventaire", title=x_title, type="temporal")
+                ],
+                )
+            .configure_scale(continuousPadding=padding)
+            .properties(width=width, height=height)
+            .interactive()
+            )
--- app/routes/generals.py
+++ app/routes/generals.py
@@ -1,32 +1,59 @@
-from flask import render_template, flash, redirect, request
+from flask import render_template
 from ..app import app
-import altair
 from ..models.graphiques import classe_graphiques
 import pandas as pd
+import csv
+from ..constantes import *
+
 
 @app.route("/")
 def accueil():
     return render_template("pages/accueil.html")
 
+
 @app.route("/cartographie")
 def cartographie():
 	return render_template("pages/cartographie.html")
+
 
 @app.route("/graphiques")
 def graphiques():
 	return render_template("pages/graphiques.html")
 
+
 @app.route("/graphiques/temporels")
 def temporels():
-	data = pd.read_csv(f"./app/statics/data/inventaire_complet.csv")
-	data["Date_inventaire"] = pd.to_datetime(data["Date_inventaire"], format="%Y/%m/%d")
+	#pour les graphiques au jour le jour
+	data_jour = pd.read_csv(CSV_GRAPHIQUES)
+	data_jour["Date_inventaire"] = pd.to_datetime(data_jour["Date_inventaire"], format="%Y/%m/%d")
 	#ajout colonne occurences
-	data["occurences"] = 1
+	data_jour["occurences"] = 1
+	general_au_jour = data_jour.groupby("Date_inventaire", as_index=False).sum()
 
-	# Group data by date
-	general = data.groupby("Date_inventaire", as_index=False).sum()
-	
-	chart = classe_graphiques.generate_temporels(general, "Mois et jour")
+	#pour les graphiques par mois
+	with open(CSV_GRAPHIQUES) as f:
+		f_o = csv.reader(f)
+		next(f_o)
+		dico_mois = {}
+		# comptage du nombre d'occurences par mois
+		for line in f_o:
+			if line[25][0:7] not in dico_mois and line[25] != "":
+				dico_mois[line[25][0:7]] = 1
+			elif line[25][0:7] in dico_mois and line[25] != "":
+				dico_mois[line[25][0:7]] = dico_mois[line[25][0:7]] + 1
+		liste_mois = []
+		dico_annee = {"01": 31, "02": 29, "03": 31, "04": 30, "05": 31, "06":30, "07": 31, "08": 31, "09": 30, "10": 31, "11": 30, "12": 31}
+		#remplissage des jours vides
+		for cle in dico_mois:
+			if cle[5:7] in dico_annee:
+				for jour in range(1, dico_annee[cle[5:7]]):
+					liste_mois.append([cle + "/"+ str(jour), dico_mois[cle]])
+		#création du dataframe pour Altair
+		data_mois = pd.DataFrame(liste_mois)
+		data_mois.columns = ["Date_inventaire", 'occurences']
+		data_mois['Date_inventaire'] = pd.to_datetime(data_mois['Date_inventaire'], format="%Y/%m")
+
+	chart = classe_graphiques.generate_temporels_bar(data_mois, "Mois et jour")
 	return chart.to_json()
 
 #@app.route("/graphiques/cumulatifs")
