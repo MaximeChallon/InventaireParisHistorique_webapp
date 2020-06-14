98ec472730d91996d06289df89f5f40d7f5800be
Maxime Challon <maxime.challon@gmail.com>
2020-06-14 18:09:05 +0200

    formulaire catalogage_form

--- app/models/formulaires.py
+++ app/models/formulaires.py
@@ -1,6 +1,7 @@
-from wtforms import RadioField, SubmitField, StringField, validators, PasswordField
+from wtforms import RadioField, SubmitField, StringField, validators, PasswordField, SelectField, IntegerField
 from flask_wtf import FlaskForm
 from .users import Classe_utilisateurs
+from ..constantes import RUE, NOM_SITE, VILLE, MOT_CLE, GENERALITE_ARHITECTURE, PHOTOGRAPHE, SUPPORT, DROITS, COULEUR, COLLECTION
 
 class Chart_form(FlaskForm):
     visuel = RadioField('Visuel', choices=[('line', 'En ligne'), ('bar', 'En barres')])
@@ -8,7 +9,7 @@ class Chart_form(FlaskForm):
     submit = SubmitField('Voir')
 
 class Forgot_form(FlaskForm):
-    email = StringField('Email Address', [validators.DataRequired(), validators.Length(min=6, max=35)])
+    email = StringField('Email Address', [validators.DataRequired(), validators.Length(min=6, max=35)], validators.Email(s))
     submit = SubmitField('Envoyer')
 
     def validate_email(self, email):
@@ -21,4 +22,39 @@ class Reset_password_form(FlaskForm):
     current_password = PasswordField("Mot de passe", [validators.DataRequired(), validators.Length(min=6, max=40)])
     confirm_password = PasswordField("Mot de passe", [validators.DataRequired(), validators.Length(min=6, max=40),
                                       validators.EqualTo('current_password')])
+    submit = SubmitField('Enregistrer')
+
+class Catalogage_form(FlaskForm):
+    N_inventaire = IntegerField("Numéro d'inventaire", [validators.DataRequired()])
+    Rue = SelectField("Rue", choices=RUE)
+    N_rue = StringField("Numéro de rue")
+    Nom_site = SelectField("Rue", choices=NOM_SITE)
+    Arrondissement = IntegerField("Arrondissement")
+    Ville = SelectField("Rue", choices=VILLE)
+    Departement = IntegerField("Code postal (2 premiers chiffres)")
+    Latitude_x = StringField("Latitude")
+    Longitude_y = StringField("Longitude")
+    Support = SelectField("Support", choices=SUPPORT)
+    Couleur = SelectField("Couleur", choices=COULEUR)
+    Taille = StringField("Taille")
+    Date_prise_vue = StringField("Date de prise de vue")
+    Photographe = SelectField("Photographe", choices=PHOTOGRAPHE)
+    Droits = SelectField("Droits", choices=DROITS)
+    Mention_don = SelectField("Don", choices=PHOTOGRAPHE)
+    Mention_collection = SelectField("Collection", choices=COLLECTION)
+    Date_construction = StringField("Date de construction")
+    Architecte = StringField("Architecte")
+    Classement_MH = StringField("Classement MH")
+    Legende = StringField("Date de prise de vue")
+    Generalite_architecture = SelectField("Généralité architecture", choices=GENERALITE_ARHITECTURE)
+    Mot_cle1 = SelectField("Mot clé", choices=MOT_CLE)
+    Mot_cle2 = SelectField("Mot clé", choices=MOT_CLE)
+    Mot_cle3 = SelectField("Mot clé", choices=MOT_CLE)
+    Mot_cle4 = SelectField("Mot clé", choices=MOT_CLE)
+    Mot_cle5 = SelectField("Mot clé", choices=MOT_CLE)
+    Mot_cle6 = SelectField("Mot clé", choices=MOT_CLE)
+    Autre_adresse = StringField("Autre adresse")
+    Notes = StringField("Notes")
+    Cote_base = StringField("Cote de la base")
+
     submit = SubmitField('Enregistrer')
