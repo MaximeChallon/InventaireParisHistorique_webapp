965fb7225ca6142fbc30a4b2c058edc79e1954a5
Maxime Challon <maxime.challon@gmail.com>
2020-07-03 18:45:17 +0200

    mise en classe du retour d'API

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,7 +1,7 @@
 from ..app import app
 from flask import render_template, request, url_for, jsonify
 from urllib.parse import urlencode
-from flask_login import login_required
+from ..models.api import Classe_API
 from..models.donnees import Classe_db
 from ..constantes import CHEMIN_API
 import re
@@ -56,14 +56,16 @@ def single_photo_id():
     Permet d'obtenir les données d'une photographie précise
     :return: JSON
     """
+    recherche = request.args.get("q", None)
+    page, results_per_page = Classe_API.check_n_p(1,1)
+    query = Classe_db.query.filter(Classe_db.N_inventaire == int(recherche))
+
     try:
-        recherche = request.args.get("q", None)
-        query = Classe_db.query.get(recherche)
-        dict_json = {"data": {str(query.N_inventaire): query.to_json()}
-        }
-        return jsonify(dict_json)
-    except:
+        resultats = query.paginate(page=page, per_page=results_per_page)
+    except Exception:
         return Json_404()
+
+    return jsonify(Classe_API.return_json(recherche, resultats, results_per_page))
 
 
 @app.route(CHEMIN_API + "/photographie/numeros_inventaire")
@@ -73,27 +75,23 @@ def multiple_photo_id():
     :return: JSON
     """
     recherche = request.args.get("q", None)
+    page = request.args.get("n", 1)
+    results_per_page = request.args.get("p", 10)
+
+    page, results_per_page = Classe_API.check_n_p(page, results_per_page)
+
     _from = int(re.sub("[^0-9]+[0-9]+$", "", recherche))
     _to = int(re.sub("[0-9]+[^0-9]+", "", recherche))
-    liste_results = []
-    while _from <= _to:
-        query = Classe_db.query.get(_from)
-        # ne retourner que les numéros d'inventaire qui ont des métadonnées; s'il est absent, il n'est pas retourné
-        if query != None:
-            liste_results.append(query)
-        _from += 1
-    #structuration des données de sortie
-    dict_results = {
-            "data": [
-                {str(photo.N_inventaire): photo.to_json()}
-                for photo in liste_results
-            ],
-            "meta": {
-                "copyright": "2020 - Association Paris Historique",
-                "total results": len(liste_results)
-            }
-        }
-    return jsonify(dict_results)
+
+    query = Classe_db.query.filter(Classe_db.N_inventaire.between(_from, _to))
+
+    try:
+        resultats = query.paginate(page=page, per_page=results_per_page)
+    except Exception:
+        return Json_404()
+
+    response = Classe_API.return_json(recherche, resultats, results_per_page)
+    return response
 
 
 @app.route(CHEMIN_API+"/photographie/adresse")
@@ -102,21 +100,11 @@ def recherche_photo_adresse():
     Recherche de photographie par adresse
     :return: JSON
     """
-    # récupération des données de l'URL
     recherche = request.args.get("q", None)
     page = request.args.get("p", 1)
     results_per_page = request.args.get("n", 10)
 
-    # gestion de la pagination et du noimbre de résultats par page
-    if isinstance(page, str) and page.isdigit():
-        page = int(page)
-    else:
-        page = 1
-
-    if isinstance(results_per_page, str) and results_per_page.isdigit():
-        results_per_page = int(results_per_page)
-    else:
-        results_per_page = 10
+    page, results_per_page = Classe_API.check_n_p(page, results_per_page)
 
     # scission de la recherche en plusieurs termes
     # de préférence, la syntaxe de la recherche est N_rue rue Code_postal? Ville?
@@ -144,7 +132,6 @@ def recherche_photo_adresse():
         Ville = ""
         Arrondissement = -1
 
-    # requêtes SQL via SQLAlchemy en fonction des données disponibles récupérées dans l'URL
     if recherche and N_rue != "-1":
         query = Classe_db.query.filter(and_(
             Classe_db.Rue.like("%{}%".format(Rue)), Classe_db.N_rue.like(N_rue))
@@ -161,41 +148,7 @@ def recherche_photo_adresse():
     except Exception:
         return Json_404()
 
-    # initialisation du dictionnaire de retour des résultats
-    dict_resultats = {
-        "links": {
-            "self": request.url
-        },
-        "data": [
-            {str(photo.N_inventaire) : photo.to_json()}
-            for photo in resultats.items
-        ],
-        "meta": {
-            "copyright": "2020 - Association Paris Historique",
-            "total results": resultats.total
-        }
-    }
-
-    # remplissage des clés de pagination du dictionnaire
-    if resultats.has_next:
-        arguments = {
-            "p": resultats.next_num,
-            "n" : results_per_page
-        }
-        if recherche:
-            arguments["q"] = recherche
-        dict_resultats["links"]["next"] = url_for("recherche_photo_adresse", _external=True)+"?"+urlencode(arguments)
-
-    if resultats.has_prev:
-        arguments = {
-            "p": resultats.prev_num,
-            "n": results_per_page
-        }
-        if recherche:
-            arguments["q"] = recherche
-        dict_resultats["links"]["prev"] = url_for("recherche_photo_adresse", _external=True)+"?"+urlencode(arguments)
-
-    response = jsonify(dict_resultats)
+    response = jsonify(Classe_API.return_json(recherche, resultats, results_per_page))
     return response
 
 
@@ -205,25 +158,14 @@ def recherche_photo_mot_cle():
     Recherche de photographie par mot clé
     :return: JSON
     """
-    # récupération des données transmises dans l'URL
     recherche = request.args.get("q", None)
     page = request.args.get("p", 1)
     results_per_page = request.args.get("n", 10)
 
-    # gestion de la pagination et du nombre de résultats par page
-    if isinstance(page, str) and page.isdigit():
-        page = int(page)
-    else:
-        page = 1
-
-    if isinstance(results_per_page, str) and results_per_page.isdigit():
-        results_per_page = int(results_per_page)
-    else:
-        results_per_page = 10
+    page, results_per_page = Classe_API.check_n_p(page, results_per_page)
 
     mot_cle = recherche.upper()
 
-    # requête dans la base de donéees avec un OR_
     if recherche:
         query = Classe_db.query.filter(or_(
             Classe_db.Mot_cle1.like("%{}%".format(mot_cle)),
@@ -236,47 +178,10 @@ def recherche_photo_mot_cle():
     else:
         query = Classe_db.query
 
-    # gestion de la pagination des résultats
     try:
         resultats = query.paginate(page=page, per_page=results_per_page)
     except Exception:
         return Json_404()
 
-    # dictionnaire d'accueil des résultats
-    dict_resultats = {
-        "links": {
-            "self": request.url
-        },
-        "data": [
-            {str(photo.N_inventaire): photo.to_json()}
-            for photo in resultats.items
-        ],
-        "meta": {
-            "copyright": "2020 - Association Paris Historique",
-            "total results": resultats.total
-        }
-    }
-
-    # remplissage de la clé de pagination dans le dictionnaire
-    if resultats.has_next:
-        arguments = {
-            "p": resultats.next_num,
-            "n": results_per_page
-        }
-        if recherche:
-            arguments["q"] = recherche
-        dict_resultats["links"]["next"] = url_for("recherche_photo_mot_cle", _external=True) + "?" + urlencode(
-            arguments)
-
-    if resultats.has_prev:
-        arguments = {
-            "p": resultats.prev_num,
-            "n": results_per_page
-        }
-        if recherche:
-            arguments["q"] = recherche
-        dict_resultats["links"]["prev"] = url_for("recherche_photo_mot_cle", _external=True) + "?" + urlencode(
-            arguments)
-
-    response = jsonify(dict_resultats)
+    response = jsonify(Classe_API.return_json(recherche, resultats, results_per_page))
     return response
