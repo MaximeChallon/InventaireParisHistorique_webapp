09b69d327378eb9b0c2a3a5be6546a9d2c40679a
Maxime Challon <maxime.challon@gmail.com>
2020-10-17 16:07:22 +0200

    possibilité de modifier ses infos

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,19 +1,25 @@
 from ..app import app, db
 from flask_login import login_required, current_user
-from flask import url_for, render_template, flash, redirect, request
+from flask import render_template, redirect, url_for, flash
 from ..models.formulaires import ModifierInformationsPersonnelles
-from ..models.users import Classe_catalogage
-from ..models.export import Classe_export
-from ..constantes import *
+from ..models.users import Classe_utilisateurs
 
 @app.route("/espace_personnel/mon_profil", methods=['GET', 'POST'])
 @login_required
 def mon_profil():
     form = ModifierInformationsPersonnelles()
+    if form.validate_on_submit():
+        current_user.nom = form.Nom.data
+        current_user.prenom = form.Prenom.data
+        current_user.mail = form.Mail.data
+        try:
+            db.session.commit()
+            flash("Informations modifiées", "info")
+            return redirect(url_for('mon_profil'))
+        except:
+            flash("Une erreur est survenue. Veuillez nous excuser.", "warning")
+        return redirect(url_for("mon_profil"))
     form.Prenom.data = current_user.prenom
     form.Nom.data = current_user.nom
     form.Mail.data = current_user.mail
-    if form.validate_on_submit():
-        pass
-    #return
     return render_template("pages/profil.html", form=form)
