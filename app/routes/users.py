25a71894fb392a9f5ceffe6d773efb5336422226
MaximeChallon <maxime.challon@gmail.com>
2021-01-31 12:40:57 +0100

    piechaart des mots-clés sur le profil utilisateur

--- app/routes/users.py
+++ app/routes/users.py
@@ -25,6 +25,45 @@ def mon_profil():
     form.Prenom.data = current_user.prenom
     form.Nom.data = current_user.nom
     form.Mail.data = current_user.mail
+
+    # utiliser plotly faire les graphiques de l'issue 62
+    mots_cles = db.session.execute("""select Mot_cle1, count(*) from Classe_db where Auteur  = '""" +
+        current_user.nom.upper() + """' and Mot_cle1 is not null and Mot_cle1 != ''
+group by Mot_cle1
+        union
+        select Mot_cle2, count(*) from Classe_db where Auteur  = '""" +
+        current_user.nom.upper() + """' and Mot_cle2 is not null and Mot_cle2 != ''
+group by Mot_cle2
+        union
+        select Mot_cle3, count(*) from Classe_db where Auteur  = '""" +
+        current_user.nom.upper() + """' and Mot_cle3 is not null and Mot_cle3 != ''
+group by Mot_cle3
+        union
+        select Mot_cle4, count(*) from Classe_db where Auteur  = '""" +
+        current_user.nom.upper() + """' and Mot_cle4 is not null and Mot_cle4 != ''
+group by Mot_cle4
+        union
+        select Mot_cle5, count(*) from Classe_db where Auteur  = '""" +
+        current_user.nom.upper() + """' and Mot_cle5 is not null and Mot_cle5 != ''
+group by Mot_cle5
+        union
+        select Mot_cle6, count(*) from Classe_db where Auteur  = '""" +
+        current_user.nom.upper() + """' and Mot_cle6 is not null and Mot_cle6 != ''
+group by Mot_cle6""").fetchall()
+    dico = {}
+    for item in mots_cles:
+        if item[0] not in  dico:
+            dico[item[0]] = item[1]
+        if item[0] in dico:
+            dico[item[0]] = dico[item[0]] + item[1]
+    liste_mots = []
+    liste_valeurs = []
+    for entree in dico:
+        liste_mots.append(entree)
+        liste_valeurs.append(dico[entree])
+    liste_finale_mots = [liste_mots, liste_valeurs]
+
     return render_template("pages/profil.html", form=form,
                            compteur_photos_ok=compteur_photos_ok,
-                           compteur_photos_attente=compteur_photos_attente)
+                           compteur_photos_attente=compteur_photos_attente,
+                           liste_mots_cles=liste_finale_mots)
